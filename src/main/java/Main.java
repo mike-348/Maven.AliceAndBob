262c4c10c243a5704e0224b77a2b948d478b834d
Mike <mike@Zipcoders-MacBook-Pro-41.local>
2022-06-27 09:17:07 -0400

    i learned how to push

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,33 @@
+import java.util.Scanner;
+
 /**
  * Created by iyasuwatts on 10/17/17.
  */
 public class Main {
 
     public static void main(String[] args ){
+
+        Scanner scan = new Scanner(System.in);
+        String enterName = "";
+        String a = "Alice";
+        String b = "Bob";
+        boolean loop = true;
+        while(loop) {
+            System.out.println("Please enter your name...");
+            enterName = scan.nextLine();
+            if (enterName.equalsIgnoreCase(a)) {
+                System.out.println("Hello, Alice.");
+                loop = false;
+            }
+            else if (enterName.equalsIgnoreCase(b)) {
+                System.out.println("Hello, Bob.");
+                loop = false;
+            }
+            else {
+                System.out.print("Name not recognized. Try again. ");
+            }
+
+        }
 
     }
 }
